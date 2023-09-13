ae2a6d0200d364867d619d666c55a77f827ecbf2
Stanislav Dolyk <tafoner2014@gmail.com>
2023-09-13 19:10:37 +0300

    Some code refactor

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen: boolean = false;
+
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+  ) {}
+
+  toggleMenu(): void {
+    const menuIcon = this.el.nativeElement.querySelector('#menu-icon');
+    const navbar = this.el.nativeElement.querySelector('.navbar');
+
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  }
+
+  closeMenu(): void {
+    const menuIcon = this.el.nativeElement.querySelector('#menu-icon');
+    const navbar = this.el.nativeElement.querySelector('.navbar');
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
+}
