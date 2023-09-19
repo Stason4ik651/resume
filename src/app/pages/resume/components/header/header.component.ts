db32917d0a354a36420f0fca15d16d79302f2f51
Stanislav Dolyk <tafoner2014@gmail.com>
2023-09-19 22:11:30 +0300

    Small improvements to the project

--- src/app/pages/resume/components/header/header.component.ts
+++ src/app/pages/resume/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +8,39 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 })
 export class HeaderComponent {
   isMenuOpen: boolean = false;
+  menuClass: string = '';
 
   constructor(
+    private router: Router,
     private renderer: Renderer2,
     private el: ElementRef,
-  ) {}
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.url;
+        this.menuClass = this.getMenuClass(url);
+      }
+    });
+  }
+
+  getMenuClass(url: string): string {
+    switch (url) {
+      case '/':
+        return 'header';
+      case '/watches':
+        return 'unfixed-header';
+      case '/calculator':
+        return 'unfixed-header';
+      case '/movies':
+        return 'unfixed-header';
+      case '/todo':
+        return 'unfixed-header';
+      case '/snake':
+        return 'unfixed-header';
+      default:
+        return '';
+    }
+  }
 
   toggleMenu(): void {
     const menuIcon = this.el.nativeElement.querySelector('#menu-icon');
