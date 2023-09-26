81dd0a3388b95b68d71311964ed05166c93fc6b6
Stanislav Dolyk <tafoner2014@gmail.com>
2023-09-26 19:34:19 +0300

    Add icons and fix some styles

--- src/app/pages/resume/components/footer/footer.component.ts
+++ src/app/pages/resume/components/footer/footer.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  constructor() {}
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(): void {
+    const scroll = document.querySelector('.upward') as HTMLElement;
+    if (scroll) {
+      scroll.classList.toggle('active', window.scrollY > 900);
+    }
+  }
+}
